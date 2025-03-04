720bcf019c6a437412e03e5fdeff1d1786f745cf
Manuel Santelices <manuel.santelices@signatureip.ai>
2025-03-04 02:07:10 -0800

    Reference: [SIGINOC-1143] As a user, I want the hdl_obfuscator to ensures that obfuscated files compiles without errors after obfuscation Description:   Added line checker for mapfile entries Notes:   - N/A   - N/A   - N/A

--- src/hdl_obfuscator/mangler/FileListVisitor.py
+++ src/hdl_obfuscator/mangler/FileListVisitor.py
@@ -1,7 +1,6 @@
 #!/bin/env python3
 import os
 import re
-import logging
 from . import config
 
 class FileListVisitor:
@@ -12,15 +11,16 @@ class FileListVisitor:
     def parse(self) -> None:
         self._source_files = self._read_file_list(self._file_list_path)
 
-    def get_source_files(self) -> set[str]:
+    def get_source_files(self) -> list[str]:
         return self._source_files
 
-    def _read_file_list(self, file_list_path) -> set[str]:
+    def _read_file_list(self, file_list_path) -> list[str]:
         source_files = set()
         with open(file_list_path, "r", encoding="utf-8") as obfuscation_list:
             for line in obfuscation_list.readlines():
                 line = line.rstrip('\n')
                 line = line.strip()
+                line = os.path.expandvars(line)
                 print(f"Reading path: {line}")
 
                 if self._is_line_commented(line):
@@ -28,12 +28,13 @@ class FileListVisitor:
 
                 if self._is_verilog_file_path(line):
                     print(f"Using Verilog file: {line}")
-                    line = os.path.expandvars(os.path.normpath(line))
-                    source_files.add((line, line))
+                    line = os.path.normpath(line)
+                    if line:
+                        source_files.add((line))
 
                 elif self._is_incdir_path(line):
                     incdir_line = self._get_file_list_from_incdir_path(line)
-                    incdir_files_path = os.path.expandvars(os.path.normpath(incdir_line))
+                    incdir_files_path = os.path.normpath(incdir_line)
 
                     if not os.path.exists(incdir_files_path):
                         print(f"Directory not found: {incdir_files_path}")
@@ -43,17 +44,17 @@ class FileListVisitor:
                         for file in files:
                             file_path = os.path.join(root, file)
                             print(f"Adding file: {file_path}")
-                            source_files.add((file_path,file_path))
+                            source_files.add((file_path))
 
                 elif self._is_file_list_file_path(line):
-                    inner_file_list_path = os.path.expandvars(os.path.normpath(self._get_file_list_from_file_path(line)))
+                    inner_file_list_path = os.path.normpath(self._get_file_list_from_file_path(line))
 
                     if not os.path.exists(inner_file_list_path):
                         print(f"File list not found: {inner_file_list_path}")
                         continue
 
                     inner_source_files = self._read_file_list(inner_file_list_path)
-                    source_files.add((inner_source_files,inner_source_files))
+                    source_files.update(inner_source_files)
 
         return source_files
 
@@ -66,7 +67,7 @@ class FileListVisitor:
 
     def _get_file_list_from_incdir_path(self, path: str) -> str:
         if path.startswith("+incdir+"):
-            return path[len("+incdir+"):]  # Remove only the `+incdir+` part
+            return path[len("+incdir+"):]
 
     def _is_file_list_file_path(self, path) -> bool:
         if 2 != len(path.split()):
